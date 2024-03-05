2f8bc634e571d94dbaaeca2f0ff25c852116ecfc
Fajar Rizky <56773371+fajar1411@users.noreply.github.com>
2024-03-05 15:51:30 +0700

    create rest api change password

--- src/main/java/com/example/demo/controller/AccountRestController.java
+++ src/main/java/com/example/demo/controller/AccountRestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.dto.ChangePassword;
 import com.example.demo.model.User;
 import com.example.demo.repository.EmployeeRepository;
